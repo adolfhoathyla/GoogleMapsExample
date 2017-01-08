483f2794f6bce7e78e56c7aeb606df7b03832080
Adolfho Athyla <adolfo.athyla@gmail.com>
2017-01-08 15:16:43 -0300

    Add basic structure to project.

---
+++ AlamofireExample/ServerHelper.swift
@@ -0,0 +1,13 @@
+//
+//  ServerHelper.swift
+//  AlamofireExample
+//
+//  Created by Adolfho Athyla on 08/01/17.
+//  Copyright © 2017 a7hyla. All rights reserved.
+//
+
+import UIKit
+
+class ServerHelper: NSObject {
+
+}
---
+++ AlamofireExample/User.swift
@@ -0,0 +1,32 @@
+//
+//  User.swift
+//  AlamofireExample
+//
+//  Created by Adolfho Athyla on 08/01/17.
+//  Copyright © 2017 a7hyla. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class User: NSObject {
+    var id: String?
+    var age: Int?
+    var name: String?
+    var company: String?
+    var email: String?
+    var phone: String?
+    var address: String?
+    var coordinates: CLLocationCoordinate2D?
+    
+    init(id: String, age: Int, name: String, company: String, email: String, phone: String, address: String, latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        self.id = id
+        self.age = age
+        self.name = name
+        self.company = company
+        self.email = email
+        self.phone = phone
+        self.address = address
+        self.coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
